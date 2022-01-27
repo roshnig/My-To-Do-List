7f66ab015f6c59ff7920c62550089482e70c21cc
Roshni <roshnigupta8@gmail.com>
2022-01-27 16:09:44 +0000

    completed basic app

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -1,33 +1,33 @@
 import React, { useState } from "react";
 
 const ToDoForm = (props) => {
-  const [task, setTask] = useState({ task: "" });
+    const [task, setTask] = useState({ task: "" });
 
-  const addTask = (event) => {
-    event.preventDefault();
-    props.setTasks((tasks) => {
-      let newTasks = [...tasks];
-      newTasks.push(task);
-      return newTasks;
-    });
-  };
-  return (
-    <form>
-      <input
-        placeholder="please add a task"
-        id="myinput"
-        value={task.task}
-        onChange={(event) => {
-          setTask((task) => {
-            let newTask = { ...task };
-            newTask.task = event.target.value;
-            return newTask;
-          });
-        }}
-      ></input>
-      <button onClick={addTask}>Add Task</button>
-    </form>
-  );
+    const addTask = (event) => {
+        event.preventDefault();
+        props.setTasks((tasks) => {
+            let newTasks = [...tasks];
+            newTasks.push(task);
+            return newTasks;
+        });
+    };
+    return (
+        <form className="inputForm">
+            <input
+                placeholder="please add a task"
+                id="myinput"
+                value={task.task}
+                onChange={(event) => {
+                    setTask((task) => {
+                        let newTask = { ...task };
+                        newTask.task = event.target.value;
+                        return newTask;
+                    });
+                }}
+            ></input>
+            <button onClick={addTask}>Add Task</button>
+        </form>
+    );
 };
 
 export default ToDoForm;
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,26 +1,24 @@
-import { useState } from "react/cjs/react.development";
-import "../App.css";
-
 const ToDoList = (props) => {
-  // here we will have useSate
+    // here we will have useSate
 
-  return (
-    <ul>
-      {props.taskList.map((task) => {
-        return (
-          <li key={task.task}>
-            <div className="list-items">
-              <p>{task.task}</p>
-              <div className="buttons">
-                <button>edit</button>
-                <button>delete</button>
-              </div>
-            </div>
-          </li>
-        );
-      })}
-    </ul>
-  );
+    return (
+        <ul>
+            {props.taskList.map((task) => {
+                return (
+                    <li key={task.task}>
+                        <div className="list-items">
+                            <input type="checkbox"></input>
+                            <p>{task.task}</p>
+                            <div className="buttons">
+                                <button>Edit</button>
+                                <button>Delete</button>
+                            </div>
+                        </div>
+                    </li>
+                );
+            })}
+        </ul>
+    );
 };
 
 export default ToDoList;
